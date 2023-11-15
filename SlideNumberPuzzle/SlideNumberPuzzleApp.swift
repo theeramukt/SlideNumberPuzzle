60811ce90f7b9c421a7186c3a0d829a6702cecb9
ChayanitL <chayanit9336@aten.co.th>
2023-11-15 22:51:56 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/SlideNumberPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberPuzzleApp.swift
+//  SlideNumberPuzzle
+//
+//  Created by ชญานิษฐ์ โลหะกิจจา on 15/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
